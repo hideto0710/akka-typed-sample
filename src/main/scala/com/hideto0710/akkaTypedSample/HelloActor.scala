2293b9b1327517407372c29bdc621b9c50491961
hideto0710 <h.inamura0710@gmail.com>
2016-01-26 23:14:12 +0900

    make HelloActor extends App

--- src/main/scala/com/hideto0710/akkaTypedSample/HelloActor.scala
+++ src/main/scala/com/hideto0710/akkaTypedSample/HelloActor.scala
@@ -1,18 +1,17 @@
 package com.hideto0710.akkaTypedSample
 
-import akka.typed.{ActorSystem, Props}
+import akka.typed.{Behavior, ActorSystem, Props}
 import akka.typed.ScalaDSL._
 
-object HelloActor {
+object HelloActor extends App {
+
   case class Message(value: String)
 
-  def main(args: Array[String]): Unit = {
-    val behavior = Static[Message] {
-      case Message(value) => println(value)
-    }
-
-    val system = ActorSystem("MySystem", Props(behavior))
-    system ! Message("Hello World")
-    system.terminate()
+  val behavior: Behavior[Message] = Static[Message] {
+    case Message(value) => println(value)
   }
+
+  val system = ActorSystem("MySystem", Props(behavior))
+  system ! Message("Hello World")
+  system.terminate()
 }
