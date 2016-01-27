6d44e46aa27fa2d93f5f86f376c53408c9f41ab7
hideto0710 <h.inamura0710@gmail.com>
2016-01-27 12:21:29 +0900

    separate Fridge and App

--- src/main/scala/com/hideto0710/akkaTypedSample/Fridge.scala
+++ src/main/scala/com/hideto0710/akkaTypedSample/Fridge.scala
@@ -1,54 +1,70 @@
 package com.hideto0710.akkaTypedSample
 
-import akka.typed.ScalaDSL._
 import akka.typed._
+import akka.typed.ScalaDSL._
 
-object Fridge extends App {
+object Fridge {
+
+  case class Result(actorRef: ActorRef[Action], result: Msg) {
+    def getMessage: String = s"{${actorRef.toString()},${result.string}}"
+  }
+
+  sealed trait Msg {
+    val string: String
+  }
+  case object Ok extends Msg {
+    override val string = "ok"
+  }
+  case class Ok(val food: String) extends Msg {
+    override val string = s"{ok,$food}"
+  }
+  case object NotFound extends Msg {
+    override val string = "not_found"
+  }
 
   sealed trait Action
-  case class Store(from: ActorRef[Result], food: String) extends Action
-  case class Take(from: ActorRef[Result], food: String) extends Action
+  case class Store(food: String, replyTo: ActorRef[Result]) extends Action
+  case class Take(food: String, replyTo: ActorRef[Result]) extends Action
 
-  def fridge(foodList: Seq[String]): Behavior[Action] = Total[Action] {
-    case Store(from, food) =>
-      from ! Ok
-      fridge(foodList :+ food)
+  def fridge(foodList: Seq[String]): Behavior[Action] = SelfAware[Action] { self =>
+    Total[Action] {
+      case Store(food, replyTo) =>
+        replyTo ! Result(self, Ok)
+        fridge(foodList :+ food)
 
-    case Take(from, food) if foodList.contains(food) =>
-      from ! Ok(food)
-      fridge(foodList.filter( _ != food ))
+      case Take(food, replyTo) if foodList.contains(food) =>
+        replyTo ! Result(self, Ok(food))
+        fridge(foodList.filter(_ != food))
 
-    case Take(from, food) if !foodList.contains(food) =>
-      from ! NotFound
-      Same
-  }
-
-  sealed trait Result
-  case object Ok extends Result
-  case class Ok(food: String) extends Result
-  case object NotFound extends Result
-
-  val handler: Behavior[Result] = Static[Result] {
-    case Ok => println("ok")
-    case Ok(food) => println(s"ok: $food")
-    case NotFound => println("not_found")
-  }
-
-  object Root {
-    val run = Full[Unit] {
-      case Sig(ctx, PreStart) =>
-        val h = ctx.spawn(Props(handler), "Handler")
-        val f = ctx.spawn(Props(fridge(Seq.empty)), "Fridge")
-        f ! Store(h, "milk")
-        f ! Store(h, "bacon")
-        f ! Take(h, "bacon")
-        f ! Take(h, "turkey")
-
-        // waiting...
-        Thread.sleep(500)
-        Stopped
+      case Take(food, replyTo) if !foodList.contains(food) =>
+        replyTo ! Result(self, NotFound)
+        Same
     }
   }
+}
 
-  val system = ActorSystem("Root", Props(Root.run))
+object FridgeApp extends App {
+
+  import Fridge._
+
+  private val handler = Static[Result] {
+    case result => println(result.getMessage)
+  }
+
+  val run: Behavior[Unit] = Full[Unit] {
+    case Sig(ctx, PreStart) =>
+      val h = ctx.spawn(Props(handler), "handler")
+      val f = ctx.spawn(Props(fridge(Seq.empty)), "fridge")
+
+      f ! Store("milk", h)
+      f ! Store("bacon", h)
+      f ! Take("bacon", h)
+      f ! Take("turkey", h)
+
+      // waiting...
+      Thread.sleep(500)
+      Stopped
+  }
+
+  val system = ActorSystem("Root", Props(run))
 }
